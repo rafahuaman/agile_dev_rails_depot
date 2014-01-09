efbc1f8e9e03de071550dcff6951be7b72a08b32
Rafael <leafar.huaman@gmail.com>
2014-01-09 06:37:00 +0000

    Task H1 Email Templates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Depot::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
+  }
+  
 end
