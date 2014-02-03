7f291116c273b0554654881a73b8f28b24db8e5a
Rafael <leafar.huaman@gmail.com>
2014-02-03 03:09:07 +0000

    Fix Production Mailer Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Depot::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
